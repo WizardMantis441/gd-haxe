25eed38e56a2c70efca2611cc00168e5ceb1daa9
WizardMantis441 <77557487+WizardMantis441@users.noreply.github.com>
2023-02-20 18:31:46 -0600

    colors

--- source/menus/MainMenuState.hx
+++ source/menus/MainMenuState.hx
@@ -13,9 +13,9 @@ class MainMenuState extends FlxState {
 		super.create();
 
 		bg = new FlxBackdrop('assets/images/backgrounds/game_bg_01_001.png', FlxAxes.X, 0, 0);
-		bg.velocity.x -= 75;
+		bg.velocity.x -= 100;
 		floor = new FlxBackdrop('assets/images/grounds/groundSquare_01_001.png', FlxAxes.X, 0, 0);
-		floor.velocity.x -= 350;
+		floor.velocity.x -= 750;
 		for (i in [bg, floor]) {
 			i.setGraphicSize(Std.int(i.width * 2));
 			i.y = FlxG.height - i.height;

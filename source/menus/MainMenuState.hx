a942551529a0bae5979fb09f7c84ef9b4f0c30f9
Landon Shields <lchammer0701@gmail.com>
2023-02-22 21:41:54 -0700

    This is 100x easier plus its not complicated

--- source/menus/MainMenuState.hx
+++ source/menus/MainMenuState.hx
@@ -13,9 +13,9 @@ class MainMenuState extends State {
 		super.create();
 		FlxSprite.defaultAntialiasing = true;
 
-		bg = new FlxBackdrop('assets/images/backgrounds/game_bg_01_001.png', FlxAxes.X, 0, 0);
+		bg = new FlxBackdrop(Paths.getGJsprites("game_bg_01", NONE, BG), FlxAxes.X, 0, 0);
 		bg.velocity.x -= 100;
-		floor = new FlxBackdrop('assets/images/grounds/groundSquare_01_001.png', FlxAxes.X, 0, 0);
+		floor = new FlxBackdrop(Paths.getGJsprites("groundSquare_01", NONE, GROUND), FlxAxes.X, 0, 0);
 		floor.velocity.x -= 750;
 		for (i in [bg, floor]) {
 			i.antialiasing = true;
--- source/utils/Paths.hx
+++ source/utils/Paths.hx
@@ -20,9 +20,25 @@ class Paths {
     public static function levels(path:String) {
         return assetsPath('levels/$path');
     }
+    /**
+        @param File - The Folder name of the GD Sprite, the rest should be handled by GJSprites, if not using GJSprites, then this does not return .png [String]
+        @param qual - The quality, meaning if it is HD or UHD, or just normal quality. [GJQualityType]
+        @param Type - The Type of sprite, ex: the Ground or Background. [GJSpriteType]
+    **/
+    public static function getGJsprites(file:String, ?qual:GJQualityType = GJQualityType.NONE, ?type:GJSpriteType = GJSpriteType.GROUND) {
+        file += '/${file}_001$qual.png';
+        trace(file);
+        trace('images/$type/$file');
+        return Assets.load(IMAGE, assetsPath('images/$type/$file'));
+    }
 }
 
 enum abstract GJSpriteType(String) to String from String {
     var GROUND = "grounds";
     var BG = "backgrounds";
+}
+enum abstract GJQualityType(String) to String from String {
+    var NONE = "";
+    var HD = "";
+    var UHD = "";
 }

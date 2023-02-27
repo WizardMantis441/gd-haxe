a3ca70fc12b0a44d09b48462a4a720a7eb02e8da
ItsLJcool <54383469+ItsLJcool@users.noreply.github.com>
2023-02-27 08:31:51 -0700

    Added A Class to make Menu Items more efficiently
    This probably will make the current state unbuildable but will fix it later as I have nothing to do rn at school so im just putting ideas that I have into here

---
+++ source/utils/GJMenuItem.hx
@@ -0,0 +1,29 @@
+package utils;
+import flixel.FlxSprite;
+import flixel.input.mouse.FlxMouseEventManager;
+
+class GJMenuSprite {
+  function new() {}
+  /*
+  * @param Name - The name of the sprite, instead of doing sprite.loadGraphic since its not a single image. Its in an XML or PLIST [String]
+  * @param xPos - [Not Required] Sets the X position of the sprite [Int / Float]
+  * @param yPos - [Not Required] Sets the Y position of the sprite [Int / Float]
+  * @param MouseDown - [Not Required] A Callback Function that runs when the mouse is clicked down on the sprite [Function]
+  * @param MouseUp - [Not Required] A Callback Function that runs when the mouse is clicked down, then released on the sprite [Function]
+  * @param MouseOver - [Not Required] A Callback Function that runs when the mouse is over the sprite [Function]
+  * @param MouseOut - [Not Required] A Callback Function that runs when the mouse is no longer over the sprite. this means even if the mouse hasn't touched the sprite at all,
+  it will still run the function. I think not sure.
+  **/
+  function addMenuSprite(name:String, ?xPos:Float = 0, ?yPos:Float = 0,
+  ?mouseDown:Void->Void = function() {}, ?mouseUp:Void->Void = function() {}, ?mouseOver:Void->Void = function() {}, ?mouseOut:Void->Void = function() {}) {
+    sprite = new FlxSprite(xPos,yPos); // since we don't have a way of loading it returns basically a HaxeFlixel Logo.
+    FlxMouseEventManager.add(sprite, mouseDown, );
+    return sprite;
+  }
+}
+
+/*
+ - So this will help make menu items like switching states or smth,
+ - Have the Sprite using FlxMouseEventManager that allows us to control specific items, like when mouse is hovering over sprite.
+ - be able to set functions in the new that will apply to the Manager
+*/
